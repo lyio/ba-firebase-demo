de5ee851cd7eb10e91de3bc935de4f654bc75949
Thomas Fiedler <thfied@gmail.com>
2016-08-19 09:06:17 +0200

    remove src for gh-pages

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class BaFirebaseDemoPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('ba-firebase-demo-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,3 +0,0 @@
-export {environment} from './environment';
-export {BaFirebaseDemoAppComponent} from './ba-firebase-demo.component';
-export {BaListDemoComponent} from './ba-list-demo.component';
